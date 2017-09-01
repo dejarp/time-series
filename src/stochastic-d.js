5f094770fd22292ea009d932a0104c959bd5b318
Paul Dejardin <dejarp@gmail.com>
2017-09-01 16:02:34 -0400

    get rid of the js files because they are generated now

--- src/stochastic-d.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const simple_moving_average_1 = require("./simple-moving-average");
-const stochastic_k_1 = require("./stochastic-k");
-function StochasticD(closeSeries, highSeries, lowSeries, periods, smoothingPeriods) {
-    return simple_moving_average_1.default(stochastic_k_1.default(closeSeries, highSeries, lowSeries, periods), smoothingPeriods);
-}
-exports.default = StochasticD;
